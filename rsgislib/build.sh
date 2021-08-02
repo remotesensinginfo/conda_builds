8c5a703db8cd9f5e4f534713b3174c3207e74d12
Pete Bunting <petebunting@mac.com>
2021-08-02 19:16:41 +0100

    Add scripts to build rsgislib v5

---
+++ rsgislib/build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+source activate "${CONDA_DEFAULT_ENV}"
+
+mkdir builddir
+cd builddir
+
+if [ `uname` == Darwin ]; then
+    
+    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
+    cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+	-D BOOST_INCLUDE_DIR=$PREFIX/include \
+	-D BOOST_LIB_PATH=$PREFIX/lib \
+	-D GDAL_INCLUDE_DIR=$PREFIX/include \
+	-D GDAL_LIB_PATH=$PREFIX/lib \
+	-D HDF5_INCLUDE_DIR=$PREFIX/include \
+	-D HDF5_LIB_PATH=$PREFIX/lib \
+	-D GSL_INCLUDE_DIR=$PREFIX/include \
+	-D GSL_LIB_PATH=$PREFIX/lib \
+	-D MUPARSER_INCLUDE_DIR=$PREFIX/include \
+	-D MUPARSER_LIB_PATH=$PREFIX/lib \
+	-D KEA_INCLUDE_DIR=$PREFIX/include \
+	-D KEA_LIB_PATH=$PREFIX/lib \
+	-D Python_EXECUTABLE=$PREFIX/bin/python \
+	-D Python_INCLUDE_DIR=$PREFIX/include/python3.6m/ \
+	-D Python_LIBRARY_RELEASE=$PREFIX/lib/libpython3.6m.dylib \
+	-D CMAKE_OSX_SYSROOT="/" \
+	-D CMAKE_OSX_DEPLOYMENT_TARGET="" \
+	-D CMAKE_VERBOSE_MAKEFILE=ON \
+        -D CMAKE_BUILD_TYPE=Debug \
+	-D CMAKE_SKIP_RPATH=ON \
+	..
+else
+    export LD_LIBRARY_PATH=$PREFIX/lib
+    cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+	-D BOOST_INCLUDE_DIR=$PREFIX/include \
+	-D BOOST_LIB_PATH=$PREFIX/lib \
+	-D GDAL_INCLUDE_DIR=$PREFIX/include \
+	-D GDAL_LIB_PATH=$PREFIX/lib \
+	-D HDF5_INCLUDE_DIR=$PREFIX/include \
+	-D HDF5_LIB_PATH=$PREFIX/lib \
+	-D GSL_INCLUDE_DIR=$PREFIX/include \
+	-D GSL_LIB_PATH=$PREFIX/lib \
+	-D MUPARSER_INCLUDE_DIR=$PREFIX/include \
+	-D MUPARSER_LIB_PATH=$PREFIX/lib \
+	-D KEA_INCLUDE_DIR=$PREFIX/include \
+	-D KEA_LIB_PATH=$PREFIX/lib \
+	-D CMAKE_VERBOSE_MAKEFILE=ON \
+	-D CMAKE_SKIP_RPATH=ON \
+	..
+fi
+
+
+make -j 4
+make install
+
+
